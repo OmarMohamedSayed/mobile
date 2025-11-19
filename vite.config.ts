fb9f612511d11758fd7c9d37cd28913449b37d01
Omar Sayed <omar.sayed@montymobile.com>
2025-11-19 13:55:37 +0200

    addd new code

--- vite.config.ts
+++ vite.config.ts
@@ -51,8 +51,20 @@
     },
     build: {
       target: 'esnext',
-      outDir: 'build',
+      outDir: 'dist',
+      minify: 'esbuild',
+      rollupOptions: {
+        output: {
+          manualChunks: undefined,
+        },
+      },
     },
+    esbuild: {
+      logOverride: { 'this-is-undefined-in-esm': 'silent' },
+      legalComments: 'none',
+      logLevel: 'silent',
+    },
+    logLevel: 'warn',
     server: {
       port: 3000,
       open: true,
